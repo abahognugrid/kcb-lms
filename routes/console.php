f6b2ff4e064c4a4fc866d9067bfeb497c482e3fc
Allan Abaho <abahoallans@gmail.com>
2026-04-01 12:16:00 +0300

    clean up logs every week

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use App\Console\Commands\ApplyLoanPenalties;
 use App\Console\Commands\AutoWriteOffAfterDays;
+use App\Console\Commands\CleanupLogs;
 use App\Console\Commands\FlagOverdueLoans;
 use App\Console\Commands\PartnerSmsReminders;
 use App\Console\Commands\PastDueLoanReminders;
@@ -14,4 +15,5 @@ Schedule::command(FlagOverdueLoans::class)->dailyAt('00:01');
 Schedule::command(ApplyLoanPenalties::class)->dailyAt('00:10'); // Run this just after flagging overdue loans
 
 Schedule::command(PastDueLoanReminders::class)->cron('0 9 */3 * *');
+Schedule::command(CleanupLogs::class)->weekly();
 // Schedule::command(AutoWriteOffAfterDays::class)->dailyAt('00:15');
